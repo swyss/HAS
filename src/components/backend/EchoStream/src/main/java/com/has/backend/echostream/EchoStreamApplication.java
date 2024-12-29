ca51bfd753a3ed1bc4dc40f0adf4a84c38f7069c
swyss <stefan.arnold.wyss@gmail.com>
2024-12-29 15:13:20 +0100

    add new project

---
+++ src/components/backend/EchoStream/src/main/java/com/has/backend/echostream/EchoStreamApplication.java
@@ -0,0 +1,13 @@
+package com.has.backend.echostream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoStreamApplication.class, args);
+	}
+
+}
---
+++ src/components/backend/EchoStream/src/test/java/com/has/backend/echostream/EchoStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.has.backend.echostream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
